1701e3e24ac2baa7e490f9cca1a0aa8ae3d9eb16
alec <150299829+Heromercer@users.noreply.github.com>
2025-10-07 19:35:18 +0000

    files backup

--- hosts/desktop/variables.nix
+++ hosts/desktop/variables.nix
@@ -10,6 +10,8 @@
     location = "Boston";
     timeZone = "America/New_York";
 
+    password = "$y$j9T$D1J7uKkSYEh78Cteb0E/x0$kFPnCCE5MR.St6JXm6L1U4ma4jkROxI3uOeJDKJj4TD";
+
     autoUpgrade = false;
     autoGarbageCollector = true;
   };
