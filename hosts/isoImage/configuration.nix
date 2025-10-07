121f6c0d3aa9ddcc5dbf7bc2706966210a1de083
alec <150299829+Heromercer@users.noreply.github.com>
2025-10-07 20:41:14 +0000

    files backup

--- hosts/isoImage/configuration.nix
+++ hosts/isoImage/configuration.nix
@@ -7,5 +7,14 @@
     "${modulesPath}/installer/cd-dvd/installation-cd-minimal.nix"
   ];
 
+  environment.systemPackages = with pkgs; [
+    neovim
+    disko
+    git
+    cfdisk
+  ];
+
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+
   nixpkgs.hostPlatform = "x86_64-linux";
 }
