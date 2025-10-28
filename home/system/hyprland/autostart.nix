dffecc57101eff25b1dae63248caa53f32503aea
alec <150299829+Heromercer@users.noreply.github.com>
2025-10-28 19:10:09 -0400

    file backup

--- home/system/hyprland/autostart.nix
+++ home/system/hyprland/autostart.nix
@@ -3,7 +3,11 @@
   config,
   ...
 }: {
-  #  wayland.windowManager.hyprland.settings.exec-once = [
-  #   "systemctl --user start hyprpolkitagent"
-  # ];
+  wayland.windowManager.hyprland.settings.exec-once = [
+    "systemctl --user start hyprpolkitagent"
+    "bash -c 'wl-paste --watch cliphist store &'"
+    "dms run"
+  ];
+
+  home.packages = with pkgs; [hyprpolkitagent];
 }
