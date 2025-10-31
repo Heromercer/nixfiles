dd2782ced679dda72bf0d52353f375fc5587d4b8
alec <150299829+Heromercer@users.noreply.github.com>
2025-10-31 11:55:57 -0400

    file backup

--- home/pkgs/default.nix
+++ home/pkgs/default.nix
@@ -1,39 +1,46 @@
 {
   config,
   pkgs,
+  lib,
   ...
-}: {
-  home.packages = with pkgs; [
-    ani-cli
-    caligula
-    catppuccin-cursors.latteLight
-    qt5.qtwayland
-    qt6.qtwayland
-    libsForQt5.qt5ct
-    qt6ct
-    hyprshot
-    hyprpicker
-    swappy
-    imv
-    wf-recorder
-    wlr-randr
-    wl-clipboard
-    brightnessctl
-    gnome-themes-extra
-    libva
-    dconf
-    wayland-utils
-    wayland-protocols
-    glib
-    direnv
-    meson
-    matugen
-    cava
-    cliphist
-    btop
-    xwayland-satellite
-    nitch
-    ntfs3g
-    xdg-user-dirs
-  ];
+}: let
+  hostname = config.var.hostname;
+in {
+  home.packages = with pkgs;
+    [
+      ani-cli
+      caligula
+      catppuccin-cursors.latteLight
+      qt5.qtwayland
+      qt6.qtwayland
+      libsForQt5.qt5ct
+      qt6ct
+      hyprshot
+      hyprpicker
+      swappy
+      imv
+      wf-recorder
+      wlr-randr
+      wl-clipboard
+      brightnessctl
+      gnome-themes-extra
+      libva
+      dconf
+      wayland-utils
+      wayland-protocols
+      glib
+      direnv
+      meson
+      matugen
+      cava
+      cliphist
+      btop
+      xwayland-satellite
+      nitch
+      ntfs3g
+      xdg-user-dirs
+    ]
+    ++ lib.optionals (hostname == "firelink") [
+      atlauncher
+    ];
 }
