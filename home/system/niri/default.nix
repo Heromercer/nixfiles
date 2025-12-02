f90ab11fe3cc9436359d1e18aaf802fbfba5a2f3
alec <150299829+Heromercer@users.noreply.github.com>
2025-12-02 12:46:02 -0500

    reformat niri file

--- home/system/niri/default.nix
+++ home/system/niri/default.nix
@@ -6,6 +6,7 @@
 }: {
   imports = [
     inputs.niri.homeModules.niri
+    ./windowRules.nix
   ];
   services.hyprpolkitagent.enable = true;
   programs.niri.settings = {
@@ -71,82 +72,6 @@
       border.enable = false;
       focus-ring.enable = false;
     };
-
-    window-rules = [
-      {
-        matches = [
-          {is-active = false;}
-        ];
-        opacity = 0.9;
-      }
-
-      {
-        matches = [
-          {app-id = "kitty";}
-        ];
-        draw-border-with-background = false;
-      }
-
-      {
-        matches = [];
-        geometry-corner-radius = {
-          top-left = 12.0;
-          top-right = 12.0;
-          bottom-left = 12.0;
-          bottom-right = 12.0;
-        };
-        clip-to-geometry = true;
-      }
-
-      {
-        matches = [
-          {app-id = "^org\\.gnome\\.";}
-        ];
-        draw-border-with-background = false;
-        geometry-corner-radius = {
-          top-left = 12.0;
-          top-right = 12.0;
-          bottom-left = 12.0;
-          bottom-right = 12.0;
-        };
-        clip-to-geometry = true;
-      }
-
-      {
-        matches = [
-          {app-id = "org.quickshell$";}
-        ];
-        open-floating = true;
-      }
-
-      {
-        matches = [
-          {app-id = "firefox";}
-          {app-id = "vesktop";}
-        ];
-        open-maximized = true;
-        open-on-output = "DP-3";
-      }
-
-      {
-        matches = [
-          {title = "Steam";}
-        ];
-        open-maximized = true;
-        open-on-output = "DP-1";
-      }
-
-      {
-        matches = [
-          {app-id = "steam";}
-        ];
-        excludes = [
-          {title = "Steam";}
-        ];
-        open-floating = true;
-        open-on-output = "DP-1";
-      }
-    ];
 
     binds = with config.lib.niri.actions; {
       "Mod+Shift+Slash".action = show-hotkey-overlay;
