2ee094579c0e072a41de221df18926fd1eb9edac
alec <150299829+Heromercer@users.noreply.github.com>
2026-01-12 17:11:47 -0500

    add user to docker group

--- nixos/virtualisation.nix
+++ nixos/virtualisation.nix
@@ -1,5 +1,10 @@
-{
+{config, ...}: let
+  username = config.var.username;
+in {
   virtualisation.docker = {
     enable = true;
+  };
+  users.users.${username} = {
+    extraGroups = ["docker"];
   };
 }
