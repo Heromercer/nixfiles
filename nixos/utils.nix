fc749dd4de167ae5447a4adc25d411b9dc764548
alec <150299829+Heromercer@users.noreply.github.com>
2026-01-16 14:08:49 -0500

    add static ip address to firelink

--- nixos/utils.nix
+++ nixos/utils.nix
@@ -12,6 +12,14 @@ in {
 
   networking = {
     networkmanager.enable = true;
+    interfaces.enp8s0.ipv4.addresses = [
+      {
+        address = "192.168.50.2";
+        prefixLength = 24;
+      }
+    ];
+    defaultGateway = "192.168.50.1";
+    nameservers = ["8.8.8.8"];
   };
 
   systemd.services.NetworkManager-wait-online.enable = false;
