0c9c9b83a9ee77b62f8d72c09a8e8789e6a423bd
alec <150299829+Heromercer@users.noreply.github.com>
2026-01-26 19:48:32 -0500

    reformat media drive to ext4 from ntfs

--- nixos/drives.nix
+++ nixos/drives.nix
@@ -2,24 +2,34 @@
   config,
   pkgs,
   ...
-}: {
+}:
+{
   fileSystems = {
     "/mnt/gamesHDD" = {
       device = "/dev/disk/by-uuid/0d946588-87ee-45d0-8b4f-2214199cd264";
       fsType = "ext4";
-      options = ["defaults" "nofail"];
+      options = [
+        "defaults"
+        "nofail"
+      ];
     };
 
     "/mnt/archive" = {
       device = "/dev/disk/by-uuid/2b318b3c-5cff-4c00-84a7-0a10fb310058";
       fsType = "ext4";
-      options = ["defaults" "nofail"];
+      options = [
+        "defaults"
+        "nofail"
+      ];
     };
 
     "/mnt/media" = {
-      device = "/dev/disk/by-uuid/2AB838C3B8388F77";
-      fsType = "ntfs-3g";
-      options = ["rw" "uid=1000" "gid=100" "nofail"];
+      device = "/dev/disk/by-uuid/fbbe779f-985b-4a97-b8f6-fbb37eb0dc43";
+      fsType = "ext4";
+      options = [
+        "defaults"
+        "nofail"
+      ];
     };
   };
 }
