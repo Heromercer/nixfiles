46b5949809a4c4d9b5c24914fb1646f8be0a1086
alec <150299829+Heromercer@users.noreply.github.com>
2026-01-29 14:12:33 -0500

    refactor host specific services

--- nixos/utils.nix
+++ nixos/utils.nix
@@ -14,24 +14,11 @@ in
 
   networking = {
     networkmanager.enable = true;
-    interfaces.enp8s0.ipv4.addresses = [
-      {
-        address = "192.168.50.2";
-        prefixLength = 24;
-      }
-    ];
-    defaultGateway = "192.168.50.1";
-    nameservers = [ "8.8.8.8" ];
   };
 
   systemd.services.NetworkManager-wait-online.enable = false;
   environment.variables = {
     EDITOR = "nvim";
-  };
-
-  boot = {
-    supportedFilesystems = [ "zfs" ];
-    zfs.extraPools = [ "games" ];
   };
 
   services = {
