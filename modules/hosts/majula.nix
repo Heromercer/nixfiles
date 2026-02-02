192df026e321d540f0e55f6a5f9e6901f923d936
alec <150299829+Heromercer@users.noreply.github.com>
2026-02-02 18:27:50 -0500

    dendritic test

--- modules/hosts/majula.nix
+++ modules/hosts/majula.nix
@@ -3,11 +3,15 @@
   flake.modules.nixos.majula =
     { pkgs, ... }:
     {
+      imports = with config.flake.modules.nixos; [
+        facter
+      ];
       boot.loader = {
         systemd-boot.enable = true;
         efi.canTouchEfiVariables = true;
       };
 
+      facter.reportPath = ./facter.json;
       system.stateVersion = "25.05";
     };
 }
