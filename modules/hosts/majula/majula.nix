fb92480846c0ac3260ce16387d752ed4431b93f5
alec <150299829+Heromercer@users.noreply.github.com>
2026-02-03 09:15:53 -0500

    dendritic test

--- modules/hosts/majula/majula.nix
+++ modules/hosts/majula/majula.nix
@@ -1,10 +1,49 @@
 { inputs, self, ... }:
 {
   flake.modules.nixos.majula =
-    { pkgs, ... }:
+    {
+      config,
+      pkgs,
+      lib,
+      ...
+    }:
     {
       boot.loader.systemd-boot.enable = true;
       boot.loader.efi.canTouchEfiVariables = true;
+
+      boot.initrd.availableKernelModules = [
+        "nvme"
+        "xhci_pci"
+        "uas"
+        "sd_mod"
+        "sdhci_pci"
+      ];
+      boot.initrd.kernelModules = [ ];
+      boot.kernelModules = [ "kvm-amd" ];
+      boot.extraModulePackages = [ ];
+
+      fileSystems."/" = {
+        device = "/dev/disk/by-uuid/a42d368a-8284-41f9-88c0-2c72b00a0d12";
+        fsType = "ext4";
+      };
+
+      fileSystems."/boot" = {
+        device = "/dev/disk/by-uuid/5CCC-70D8";
+        fsType = "vfat";
+        options = [
+          "fmask=0077"
+          "dmask=0077"
+        ];
+      };
+
+      swapDevices = [
+        { device = "/dev/disk/by-uuid/262d9a59-e95f-435c-8649-91f2c0528677"; }
+      ];
+
+      networking.useDHCP = lib.mkDefault true;
+
+      nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+      hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
       system.stateVersion = "25.05";
     };
