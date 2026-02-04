cd7c2a0ec03a64d2a353f802b7ba82811db301f2
alec <150299829+Heromercer@users.noreply.github.com>
2026-02-04 14:02:13 -0500

    remove den

--- modules/den.nix
+++
@@ -1,8 +0,0 @@
-{ inputs, den, ... }:
-{
-  systems = builtins.attrNames den.hosts;
-  imports = [ inputs.den.flakeModule ];
-
-  den.hosts.x86_64-linux.igloo.users.tux = { };
-  den.aspects.igloo = { };
-}
