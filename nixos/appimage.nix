2c2e9ff3a6f1ddf9aa5dbe462cf2c75bc6eab905
alec <150299829+Heromercer@users.noreply.github.com>
2026-02-06 13:29:09 -0500

    fix syntax

--- nixos/appimage.nix
+++ nixos/appimage.nix
@@ -3,7 +3,7 @@
   programs.appimage = {
     enable = true;
     binfmt = true;
-    programs.appimage.package = pkgs.appimage-run.override {
+    package = pkgs.appimage-run.override {
       extraPkgs = pkgs: [
         pkgs.freetype
       ];
