81fc9b1fdcbf2ae8a786bed96308e991cb2b60bf
alec <150299829+Heromercer@users.noreply.github.com>
2026-02-06 13:28:25 -0500

    add appimage dependency for skyrim

--- nixos/appimage.nix
+++ nixos/appimage.nix
@@ -3,5 +3,10 @@
   programs.appimage = {
     enable = true;
     binfmt = true;
+    programs.appimage.package = pkgs.appimage-run.override {
+      extraPkgs = pkgs: [
+        pkgs.freetype
+      ];
+    };
   };
 }
