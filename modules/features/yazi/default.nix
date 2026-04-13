658faeab281db9417233bc8aa090b3e8753020a5
alec <150299829+Heromercer@users.noreply.github.com>
2026-04-13 16:29:16 -0400

    add chmod plugin to yazi

--- modules/features/yazi/default.nix
+++ modules/features/yazi/default.nix
@@ -8,53 +8,67 @@
   den.ctx.user.includes = [ mercer.yazi ];
 
   mercer.yazi = {
-    homeManager = {
-      programs.yazi = {
-        enable = true;
-        enableFishIntegration = true;
-        settings.mgr = {
-          show_hidden = true;
-        };
-        keymap = {
-          mgr.prepend_keymap = [
-            {
-              run = "cd /mnt/archive/disk/";
-              on = [
-                "g"
-                "m"
-                "1"
-              ];
-            }
+    homeManager =
+      { pkgs, ... }:
+      {
+        programs.yazi = {
+          enable = true;
+          plugins = {
+            chmod = pkgs.yaziPlugins.chmod;
+          };
+          enableFishIntegration = true;
+          settings.mgr = {
+            show_hidden = true;
+          };
+          keymap = {
+            mgr.prepend_keymap = [
+              {
+                run = "cd /mnt/archive/disk/";
+                on = [
+                  "g"
+                  "m"
+                  "1"
+                ];
+              }
 
-            {
-              run = "cd /mnt/gamesHDD/";
-              on = [
-                "g"
-                "m"
-                "2"
-              ];
-            }
+              {
+                run = "cd /mnt/gamesHDD/";
+                on = [
+                  "g"
+                  "m"
+                  "2"
+                ];
+              }
 
-            {
-              run = "cd /mnt/media/";
-              on = [
-                "g"
-                "m"
-                "3"
-              ];
-            }
+              {
+                run = "cd /mnt/media/";
+                on = [
+                  "g"
+                  "m"
+                  "3"
+                ];
+              }
 
-            {
-              run = "cd /mnt/games/disk/";
-              on = [
-                "g"
-                "m"
-                "4"
-              ];
-            }
-          ];
+              {
+                run = "cd /mnt/games/disk/";
+                on = [
+                  "g"
+                  "m"
+                  "4"
+                ];
+              }
+
+              {
+                run = "plugin chmod";
+                on = [
+                  "c"
+                  "m"
+                ];
+              }
+
+            ];
+          };
         };
       };
-    };
   };
 }
