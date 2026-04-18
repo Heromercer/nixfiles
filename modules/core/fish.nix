282aefec7dd03767e6f75129ab1abcbaa360036a
alec <150299829+Heromercer@users.noreply.github.com>
2026-04-18 09:52:55 -0400

    change nh clean command

--- modules/core/fish.nix
+++ modules/core/fish.nix
@@ -19,7 +19,7 @@
         shellAliases = {
           nhs = "nh os switch";
           nhsu = "nh os switch -u";
-          nhc = "nh clean all --keep 10 --dry";
+          nhc = "nh clean all --keep 10 --ask";
           check = "sudo nix flake check";
           nf = "cd .nixfiles";
         };
