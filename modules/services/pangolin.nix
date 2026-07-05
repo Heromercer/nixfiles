cc9c69c020c521e201dcfa8fb3bcd2fa831bd49c
alec <150299829+Heromercer@users.noreply.github.com>
2026-07-05 11:02:25 -0400

    edit pangolin config

--- modules/services/pangolin.nix
+++ modules/services/pangolin.nix
@@ -3,6 +3,16 @@
     nixos = {
       services.pangolin = {
         enable = true;
+        settings = {
+          flags = {
+            disable_signup_without_invite = true;
+            disable_user_create_org = true;
+          };
+        };
+        baseDomain = "heromercer.com";
+        letsEncryptEmail = "heromercer@gmail.com";
+        openFirewall = true;
+        dnsProvider = "cloudflare";
       };
     };
   };
