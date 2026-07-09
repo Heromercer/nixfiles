6eee02bfbf2c83fb4064ad8b4565c0354c56381d
alec <150299829+Heromercer@users.noreply.github.com>
2026-07-09 10:42:18 -0400

    add group to homepage

--- modules/services/homepage.nix
+++ modules/services/homepage.nix
@@ -5,6 +5,7 @@
         homepage-dashboard = {
           enable = true;
           openFirewall = true;
+          allowedHosts = "localhost:8082,127.0.0.1:8082,192.168.50.2:8082,100.74.74.58:8082";
           widgets = [
             {
               resources = {
@@ -12,6 +13,18 @@
                 disk = "/";
                 memory = true;
               };
+            }
+          ];
+          services = [
+            {
+              "Group 1" = [
+                {
+                  "Soul Sync" = {
+                    description = "Soul Sync Music";
+                    href = "http://100.74.74.58:8008";
+                  };
+                }
+              ];
             }
           ];
         };
