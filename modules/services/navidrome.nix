01287cd9459e59e7d5e1c1db2bdb2a4215e23fb8
alec <150299829+Heromercer@users.noreply.github.com>
2026-07-29 20:00:38 -0400

    change music folder

--- modules/services/navidrome.nix
+++ modules/services/navidrome.nix
@@ -5,11 +5,9 @@
       {
         services.navidrome = {
           enable = true;
-          user = "alec";
-          group = "users";
           settings = {
             Address = "0.0.0.0";
-            MusicFolder = "/music/";
+            MusicFolder = "/media/music/";
           };
         };
         networking.firewall.allowedTCPPorts = [ 4533 ];
