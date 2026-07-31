614b589729755ae2c0b4c9d0a552b3cfd7a80d2d
alec <150299829+Heromercer@users.noreply.github.com>
2026-07-31 10:27:55 -0400

    work on arr aspect

--- modules/features/nixflix.nix
+++ modules/features/nixflix.nix
@@ -1,23 +1,52 @@
-{ inputs, ... }:
+{
+  inputs,
+  ...
+}:
 {
   mercer.arr = {
-    nixos = {
-      imports = [ inputs.nixflix.nixosModules.default ];
+    nixos =
+      { config, ... }:
+      {
+        sops.secrets = {
+          "nixflix/jellyfin/admin_password" = { };
+          "nixflix/jellyfin/api_key" = { };
+          "nixflix/seerr/api_key" = { };
+        };
 
-      nixflix = {
-        enable = true;
-        mediaDir = "/data/media/";
-        stateDir = "/media/.state";
+        imports = [ inputs.nixflix.nixosModules.default ];
 
-        nginx.enable = true;
-
-        jellyfin = {
+        nixflix = {
           enable = true;
-          users.admin = {
-            policy.isAdministrator = true;
+          mediaDir = "/data/media";
+          stateDir = "/data/.state";
+          mediaUsers = [ "alec" ];
+
+          theme = {
+            enable = true;
+            name = "overseerr";
+          };
+
+          nginx = {
+            enable = true;
+            addHostsEntries = true;
+          };
+
+          postgres.enable = true;
+
+          jellyfin = {
+            enable = true;
+            apiKey._secret = config.sops.secrets."nixflix/jellyfin/api_key".path;
+            users.admin = {
+              policy.isAdministrator = true;
+              password._secret = config.sops.secrets."nixflix/jellyfin/admin_password".path;
+            };
+          };
+
+          seerr = {
+            enable = true;
+            apiKey._secret = config.sops.secrets."nixflix/seerr/api_key".path;
           };
         };
       };
-    };
   };
 }
