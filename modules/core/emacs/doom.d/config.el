df6edcbb250df95d632ee3048698919ba404e4f4
alec <150299829+Heromercer@users.noreply.github.com>
2026-09-06 10:07:09 -0400

    change org headers font and add comment keybind

--- modules/core/emacs/doom.d/config.el
+++ modules/core/emacs/doom.d/config.el
@@ -7,6 +7,21 @@
 (setq confirm-kill-emacs nil)
 (setq catppuccin-flavor 'macchiato)
 
+(custom-theme-set-faces!
+  'catppuccin
+  '(org-level-1 :inherit outline-1 :height 1.6)
+  '(org-level-2 :inherit outline-2 :height 1.5)
+  '(org-level-3 :inherit outline-3 :height 1.4)
+  '(org-level-4 :inherit outline-4 :height 1.3)
+  '(org-level-5 :inherit outline-5 :height 1.2)
+  '(org-level-6 :inherit outline-6 :height 1.1)
+  '(org-level-7 :inherit outline-7 :height 1.0)
+  '(org-level-8 :inherit outline-8 :height 1.0)
+  '(org-document-title :height 1.8 :bold t :underline nil))
+
+(map! :leader
+      :desc "Comment line" "-" #'comment-line)
+
 (after! org-roam
   (setq org-roam-capture-templates
         '(
