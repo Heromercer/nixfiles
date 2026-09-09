61e01eefcebdfff244932c847e6868f251da2a55
alec <150299829+Heromercer@users.noreply.github.com>
2026-09-09 19:10:31 -0400

    fix nixflix secrets

--- modules/features/nixflix.nix
+++ modules/features/nixflix.nix
@@ -7,11 +7,15 @@
     nixos =
       { config, ... }:
       {
-        sops.secrets = {
-          "nixflix/jellyfin/admin_password" = { };
-          "nixflix/jellyfin/api_key" = { };
-          "nixflix/seerr/api_key" = { };
-        };
+        sops.secrets =
+          let
+            sopsFile = ../../secrets.yaml;
+          in
+          {
+            "nixflix/jellyfin/admin_password" = { inherit sopsFile; };
+            "nixflix/jellyfin/api_key" = { inherit sopsFile; };
+            "nixflix/seerr/api_key" = { inherit sopsFile; };
+          };
 
         imports = [ inputs.nixflix.nixosModules.default ];
 
@@ -33,6 +37,13 @@
 
           postgres.enable = true;
 
+          sonarr = {
+            enable = true;
+            config = {
+              apikey._secret = config.sops.secrets."nixflix/sonarr/api_key.path";
+            };
+          };
+
           jellyfin = {
             enable = true;
             apiKey._secret = config.sops.secrets."nixflix/jellyfin/api_key".path;
@@ -42,10 +53,10 @@
             };
           };
 
-          seerr = {
-            enable = true;
-            apiKey._secret = config.sops.secrets."nixflix/seerr/api_key".path;
-          };
+          # seerr = {
+          #   enable = true;
+          #   apiKey._secret = config.sops.secrets."nixflix/seerr/api_key".path;
+          # };
         };
       };
   };
