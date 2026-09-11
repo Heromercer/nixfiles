73bf57c0398da292bc68d3fa3c33ae72d0741ef6
alec <150299829+Heromercer@users.noreply.github.com>
2026-09-11 12:43:07 -0400

    make greeter mouse match wm

--- modules/features/noctalia.nix
+++ modules/features/noctalia.nix
@@ -1,36 +1,44 @@
 { __findFile, inputs, ... }:
 {
   mercer.noctalia = {
-    includes = [ <mercer/services/power-mngmt> ];
+    includes = [
+      <mercer/services/power-mngmt>
+      <mercer/cursor>
+    ];
 
-    nixos = {
-      imports = [
-        inputs.noctalia.nixosModules.default
-        inputs.noctalia-greeter.nixosModules.default
-      ];
+    nixos =
+      { pkgs, ... }:
+      {
+        imports = [
+          inputs.noctalia.nixosModules.default
+          inputs.noctalia-greeter.nixosModules.default
+        ];
 
-      programs.noctalia = {
-        enable = true;
-        systemd.enable = true;
+        programs.noctalia = {
+          enable = true;
+          systemd.enable = true;
 
-        recommendedServices.enable = true;
-      };
+          recommendedServices.enable = true;
+        };
 
-      programs.noctalia-greeter = {
-        enable = true;
-        greeter-args = "";
-        settings = {
+        programs.noctalia-greeter = {
+          enable = true;
+          settings = {
+            cursor = {
+              theme = "catppuccin-latte-light-cursors";
+              size = 24;
+              path = "${pkgs.catppuccin-cursors.latteLight}/share/icons";
+            };
+          };
+        };
 
+        nix.settings = {
+          extra-substituters = [ "https://noctalia.cachix.org" ];
+          extra-trusted-public-keys = [
+            "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
+          ];
         };
       };
-
-      nix.settings = {
-        extra-substituters = [ "https://noctalia.cachix.org" ];
-        extra-trusted-public-keys = [
-          "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
-        ];
-      };
-    };
 
     homeManager = {
       imports = [ inputs.noctalia.homeModules.default ];
